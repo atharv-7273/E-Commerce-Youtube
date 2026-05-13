85bbe5d72c9d2316efbf3c3b8fa394209dbd9e92
Atharv <atharvkumkar7273@gmail.com>
2026-05-14 00:47:02 +0530

    Web Working Properly Until 10:30 You Start next Create Order Page on 14th MAY

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,29 +1,49 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+
+//   password: {
+//     type: String,
+//     required: true
+//   },
+
+//   cartData: {
+//     type: Object,
+//     default: {}
+//   }
+
+// }, { minimize: false });
+
+// const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+
+// export default userModel;
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
+  name: { type: String, required: true },
 
-  password: {
-    type: String,
-    required: true
-  },
+  email: { type: String, required: true, unique: true },
 
-  cartData: {
-    type: Object,
-    default: {}
-  }
+  password: { type: String, required: true },
 
-}, { minimize: false });
+  cartData: { type: Object, default: {} }
 
-const userModel = mongoose.models.user || mongoose.model('user', userSchema);
+});
+
+const userModel =
+  mongoose.models.user ||
+  mongoose.model("user", userSchema);
 
 export default userModel;
